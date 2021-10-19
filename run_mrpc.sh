de4614d4c70f85e8ed1a2b29e57b9b1820b69745
MarsJacobs_NIPA <niceball0827@hanyang.ac.kr>
2021-10-19 21:36:47 +0900

    1019

--- run_mrpc.sh
+++ run_mrpc.sh
@@ -29,7 +29,7 @@ q_ffn_2=1
 q_emb=1
 q_cls=1
 
-CUDA_VISIBLE_DEVICES=$1 python quant_task_glue.py --data_dir data --task_name mrpc --output_dir output --learning_rate 2e-5 --num_train_epochs 3 \
+CUDA_VISIBLE_DEVICES=$1 python quant_task_glue.py --data_dir data --task_name rte --output_dir output --learning_rate 2e-5 --num_train_epochs 3 \
 --weight_bits 2 --input_bits 8 --aug_train --kd_layer_num ${kd_layer_num} --save_quantized_model \
 --gpu 1 --quantize ${quantize} --act_quant ${act_quant} --qkv ${q_qkv} --ffn_1 ${q_ffn_1} --ffn_2 ${q_ffn_2} --emb ${q_emb} --cls ${q_cls} \
 --layer_num ${layer_num} \
@@ -43,7 +43,7 @@ q_ffn_2=0
 q_emb=1
 q_cls=1
 
-CUDA_VISIBLE_DEVICES=$1 python quant_task_glue.py --data_dir data --task_name mrpc --output_dir output --learning_rate 2e-5 --num_train_epochs 3 \
+CUDA_VISIBLE_DEVICES=$1 python quant_task_glue.py --data_dir data --task_name rte --output_dir output --learning_rate 2e-5 --num_train_epochs 3 \
 --weight_bits 2 --input_bits 8 --aug_train --kd_layer_num ${kd_layer_num} --save_quantized_model \
 --gpu 1 --quantize ${quantize} --act_quant ${act_quant} --qkv ${q_qkv} --ffn_1 ${q_ffn_1} --ffn_2 ${q_ffn_2} --emb ${q_emb} --cls ${q_cls} \
 --layer_num ${layer_num} \
@@ -57,7 +57,7 @@ q_ffn_2=1
 q_emb=0
 q_cls=1
 
-CUDA_VISIBLE_DEVICES=$1 python quant_task_glue.py --data_dir data --task_name mrpc --output_dir output --learning_rate 2e-5 --num_train_epochs 3 \
+CUDA_VISIBLE_DEVICES=$1 python quant_task_glue.py --data_dir data --task_name rte --output_dir output --learning_rate 2e-5 --num_train_epochs 3 \
 --weight_bits 2 --input_bits 8 --aug_train --kd_layer_num ${kd_layer_num} --save_quantized_model \
 --gpu 1 --quantize ${quantize} --act_quant ${act_quant} --qkv ${q_qkv} --ffn_1 ${q_ffn_1} --ffn_2 ${q_ffn_2} --emb ${q_emb} --cls ${q_cls} \
 --layer_num ${layer_num} \
@@ -71,7 +71,7 @@ q_ffn_2=1
 q_emb=1
 q_cls=0
 
-CUDA_VISIBLE_DEVICES=$1 python quant_task_glue.py --data_dir data --task_name mrpc --output_dir output --learning_rate 2e-5 --num_train_epochs 3 \
+CUDA_VISIBLE_DEVICES=$1 python quant_task_glue.py --data_dir data --task_name rte --output_dir output --learning_rate 2e-5 --num_train_epochs 3 \
 --weight_bits 2 --input_bits 8 --aug_train --kd_layer_num ${kd_layer_num} --save_quantized_model \
 --gpu 1 --quantize ${quantize} --act_quant ${act_quant} --qkv ${q_qkv} --ffn_1 ${q_ffn_1} --ffn_2 ${q_ffn_2} --emb ${q_emb} --cls ${q_cls} \
 --layer_num ${layer_num} \
